2ca8773361bb732378900a602ed855af5da3d942
nikolarank <nikolarank94@gmail.com>
2016-06-05 17:27:12 +0200

    a

---
+++ Adiutor Projekat/Adiutor MVC/Bussines/DTO/Predlozeni_TagDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines.DTO
+{
+    public class Predlozeni_TagDTO
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string TagIme { get; set; }
+        public string Opis { get; set; }
+        public DateTime DatumPostavljanja { get; set; }
+        public DateTime DatumObrade { get; set; }
+    }
+}
