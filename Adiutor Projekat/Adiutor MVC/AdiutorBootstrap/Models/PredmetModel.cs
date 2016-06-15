c36ab222c4595446934370d1bdcb7addec35f7e2
nikolarank <nikolarank94@gmail.com>
2016-06-15 18:02:35 +0200

    Stranica Predmet

---
+++ Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Models/PredmetModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AdiutorBootstrap.Models
+{
+    public class PredmetModel
+    {
+        [Display(Name="Naziv predmeta")]
+        public string NazivPredmeta { get; set; }
+
+        [Display(Name = "Godina studija")]
+        public decimal GodinaStudija { get; set; }
+
+        [Display(Name="Semestar")]
+        public decimal Semestar { get; set; }
+
+        [Display(Name ="Zaduzeni profesor")]
+        public string ZaduzeniProfesor { get; set; }
+
+        [Display(Name = "Oblasti")]
+        public IList<OblastModel> Oblasti { get; set; }
+
+        public PredmetModel()
+        {
+            Oblasti = new List<OblastModel>();
+        }
+
+    }
+}
