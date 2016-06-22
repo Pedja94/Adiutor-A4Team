b69f1663f3046e029db41d3a8225728e85345fea
MilosMladenovic <milos_misa@hotmail.com>
2016-06-22 20:54:25 +0200

    Pretrazivanje  po tagovima
    Pretrazivanje po tagovima, nalazi se u oblasticontroller

--- Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Controllers/OblastiController.cs
+++ Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Controllers/OblastiController.cs
@@ -35,7 +35,86 @@ namespace AdiutorBootstrap.Controllers
         //    return View("~/Views/PitanjeIOdgovori/PitanjeIOdgovori.cshtml", model);
         //}
 
+        public ActionResult TrazenjePoTagu(string tagovi){
 
+            string primljeniTagovi = tagovi;
+            List<PitanjeDTO> PronadjenaPitanja = new List<PitanjeDTO>();
+
+            if (primljeniTagovi[0] == '#')
+            {
+                //ovde smo ako pretrazujemo po tagovima 
+                char [] separatingChar = {'#',' '};
+                string[] nizTagova = primljeniTagovi.Split(separatingChar, System.StringSplitOptions.RemoveEmptyEntries);
+
+                List<PitanjeDTO> pitanja = Pitanja.VratiSvaPitanjaTaga(nizTagova[0]);
+
+                string[] tagoviPitanja = new string[pitanja.Count];
+                for (int k=0; k<tagoviPitanja.Length;k++)
+                {
+                    tagoviPitanja[k] = "";
+                }
+    
+
+                int i=0;
+                foreach (var pitanje in pitanja)
+                {
+                    List<TagDTO> Tagovi = Pitanja.VratiSveTagovePitanja(pitanje.Id);
+                    foreach (var tag in Tagovi)
+                    {
+                        tagoviPitanja[i] = ""+tagoviPitanja[i]+tag.TagIme + " ";
+                 
+                    }
+                    i++;
+                }
+
+                //treba da konvertujemo listu pitanja u niz pitanja
+
+                PitanjeDTO[] pitanjaNiz = pitanja.ToArray();
+
+                int j = 0;
+                for (int petlja = 0; petlja < tagoviPitanja.Length; petlja++)
+                {
+                    int brojac = 0;
+                    foreach (var tag in nizTagova)
+                    {
+                        if (tagoviPitanja[j].Contains(tag))
+                        {
+                            brojac++;
+                            if (brojac == nizTagova.Length)
+                            {
+                                PronadjenaPitanja.Add(pitanjaNiz[j]);
+                            }
+                        }
+                    }
+                    j++;
+                }
+                
+                       
+                OblastModel oblast = new OblastModel();
+                oblast.Naziv="Nesto";
+
+                foreach(var pit in PronadjenaPitanja)
+                {
+                    KorisnikDTO autorPitanja=Korisnici.Procitaj(pit.KorisnikId);
+                    PitanjeModel p =new PitanjeModel()
+                    {
+                            Id = pit.Id,
+                            DatumVreme = pit.DatumVreme,
+                            Text = pit.Tekst,
+                            AutorPitanja = autorPitanja.Ime,
+                            AutorId=autorPitanja.Id,
+                    };
+                    oblast.Pitanja.ListaPitanja.Add(p);
+                
+                }
+                return View("~/Views/Oblasti/Oblasti.cshtml", oblast);
+            }
+            else
+            {
+                return View("~/Home/Pocetna");
+            }
+
+        }
 
 	}
 }
