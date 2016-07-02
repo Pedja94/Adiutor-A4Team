defbeb3caa3c03e4c8d9cf60410548ff5b727e24
Pedja94 <djape94@gmail.com>
2016-07-02 20:57:09 +0200

    nesto bzv

---
+++ Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Controllers/AdministracijaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdiutorBootstrap.Controllers
+{
+    public class AdministracijaController : Controller
+    {
+        //
+        // GET: /Administracija/
+        public ActionResult Administracija()
+        {
+            return View();
+        }
+	}
+}
