6f52bd79081196a80930c1203ca913440d36c223
radmarko <marko.radosavljevic94@gmail.com>
2016-07-03 10:14:48 +0200

    Nes

--- Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Controllers/AdministracijaController.cs
+++ Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Controllers/AdministracijaController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdiutorBootstrap.Models;
+using Business.DataAccess;
+using Business.DTO;
 
 namespace AdiutorBootstrap.Controllers
 {
@@ -13,6 +16,24 @@ namespace AdiutorBootstrap.Controllers
         public ActionResult Administracija()
         {
             return View();
+        }
+
+        public ActionResult AdministracijaSmerova()
+        {
+            List<SmerModel> model = new List<SmerModel>();
+            List<SmerDTO> smerovi = Smerovi.VratiSve();
+
+            foreach (SmerDTO smer in smerovi)
+            {
+                model.Add(new SmerModel() {
+                    Id = smer.Id,
+                    PocSem = smer.PocSem,
+                    KrajSem = smer.KrajSem,
+                    Ime = smer.Ime
+                });
+            }
+
+            return View(model);
         }
 	}
 }
---
+++ Adiutor Projekat/Adiutor MVC/AdiutorBootstrap/Models/SmerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdiutorBootstrap.Models
+{
+    public class SmerModel
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public int PocSem { get; set; }
+        public int KrajSem { get; set; }
+    }
+}
