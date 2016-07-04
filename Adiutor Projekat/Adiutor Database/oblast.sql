9656fa4cf8b68910e834824aee3e08597398bbec
nikolarank <nikolarank94@gmail.com>
2016-07-04 03:49:14 +0200

    podaci

---
+++ Adiutor Projekat/Adiutor Database/oblast.sql
@@ -0,0 +1,17 @@
+INSERT INTO OBLAST (IME, PREDMET_ID, OPIS)
+VALUES ('HTML', 128, 'HTML (engl. HyperText Markup Language, jezik za označavanje hiperteksta) je opisni jezik specijalno namenjen opisu veb stranica. Pomoću njega se jednostavno mogu odvojiti elementi kao što su naslovi, paragrafi, citati i slično. Pored toga, u HTML standard su ugrađeni elementi koji detaljnije opisuju sam dokument kao što su kratak opis dokumenta, ključne reči, podaci o autoru i slično. Ovi podaci su opštepoznati kao meta podaci i jasno su odvojeni od sadržaja dokumenta.');
+
+INSERT INTO OBLAST (IME, PREDMET_ID, OPIS)
+VALUES ('CSS', 128, 'CSS (engl. Cascading Style Sheets) je jezik formatiranja pomoću kog se definiše izgled elemenata veb-stranice.
+Prvobitno, HTML je služio da definiše kompletan izgled, strukturu i sadržaj veb-stranice, ali je od verzije 4.0 HTML-a uveden CSS koji bi definisao konkretan izgled, dok je HTML ostao u funkciji definisanja strukture i sadržaja.');
+
+INSERT INTO OBLAST (IME, PREDMET_ID, OPIS)
+VALUES ('JAVASCRIPT', 128, 'Džavaskript ili Javaskript (engl. JavaScript) je skriptni programski jezik koji se prvenstveno koristi za definisanje funkcionalnosti veb stranica na klijentskoj strani. Dinamičan, slabo tipiziran jezik, sa skromnom podrškom za objektno orijentisano programiranje, on ustvari predstavlja implementaciju standarda Ekmaskript.
+Iako je prvobitna ideja bila da Javaskript liči na Javu[1], on ne sadrži nijedan ključni element Jave osim onih koje su oba nasledili iz C-a. Jezik je najpoznatiji po programiranju klijentske funkcionalnosti veb stranica, ali se može koristiti i kao skriptni jezik za pristup objektima u drugim aplikacijama.
+Jezik se prvo zvao „Lajvskript“ (engl. LiveScript), ali je ime promenjeno u Javaskript, zbog marketinškog dogovora između Netskejpa i Sana. Ključni elementi dizajna Javaskripta su nasleđeni iz programskog jezika Self.');
+
+INSERT INTO OBLAST (IME, PREDMET_ID, OPIS)
+VALUES ('AJAX', 128, 'Ajaks (još AJAX; skraćenica za Asinhroni JavaScript i XML)[1] je grupa međusobno povezanih tehnika za razvoj veba korišćenih na klijentskoj strani za pravljenje sinhronih i asinhronih veb aplikacija. Korišćenjam Ajaksa, veb aplikacije mogu da šalju i primaju podatke sa servera asinhrono (u pozadini) bez menjanja trenutnog prikaza i ponašanja stranice. Podaci mogu biti preuzeti pomoću objekta XMLHttpRequest. Bez obzira na ime, korišćenje XML-a nije obavezno (JSON se često koristi), a zahtevi ne moraju biti asinhroni.[2]');
+
+INSERT INTO OBLAST (IME, PREDMET_ID, OPIS)
+VALUES ('PHP', 128, 'Pi-Ejč-Pi ili Pe-Ha-Pe (PHP: Hypertext Preprocessor) specijalizovani je skriptni jezik prvenstveno namenjen za izradu dinamičnog veb sadržaja i izvodi se na strani servera.');
---
+++ Adiutor Projekat/Adiutor Database/profesori.sql
@@ -0,0 +1,50 @@
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('ivica', 'markovic', 'ivica.markovic@adiutor.com', 'ivica', 'markovic', 1, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('aleksandar', 'dimitrijevic', 'aleksandar.dimitrijevic@adiutor.com', 'aleksandar', 'dimitrijevic', 2, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('aleksandar', 'stanimirovic', 'aleksandar.stanimirovic@adiutor.com', 'aleksandar', 'stanimirovic', 3, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('martin', 'jovanovic', 'martin.jovanovic@adiutor.com', 'martin', 'jovanovic', 4, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('milos', 'radmanovic', 'milos.radmanovic@adiutor.com', 'milos', 'radmanovic', 5, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('milos', 'bogdanovic', 'milos.bogdanovic@adiutor.com', 'milos', 'bogdanovic', 6, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('milun', 'jevtic', 'milun.jevtic@adiutor.com', 'milun', 'jevtic', 7, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('oliver', 'vojinovic', 'oliver.vojinovic@adiutor.com', 'oliver', 'vojinovic', 8, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('dragan', 'stojanovic', 'dragan.stojanovic@adiutor.com', 'dragan', 'stojanovic', 9, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('branislav', 'randjelovic', 'branislav.randjelovic@adiutor.com', 'branislav', 'randjelovic', 10, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('vladan', 'mihajlovic', 'vladan.mihajlovic@adiutor.com', 'vladan', 'mihajlovic', 11, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('petar', 'rajkovic', 'petar.rajkovic@adiutor.com', 'petar', 'rajkovic', 12, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('vladimir', 'stankovic', 'vladimir.stankovic@adiutor.com', 'vladimir', 'stankovic', 13, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('dragan', 'jankovic', 'dragan.jankovic@adiutor.com', 'dragan', 'jankovic', 14, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('dejan', 'rancic', 'dejan.rancic@adiutor.com', 'dejan', 'rancic', 15, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('aleksandar', 'milosavljevic', 'aleksandar.milosavljevic@adiutor.com', 'aleksandar', 'milosavljevic', 16, 2, 1);
+
+INSERT INTO KORISNIK (USERNAME, PASSWORD, EMAIL, IME, PREZIME, BROJ_INDEKSA, ROLE_ID, STATUS_ID)
+VALUES ('ivan', 'petkovic', 'ivan.petkovic@adiutor.com', 'ivan', 'petkovic', 17, 2, 1);
